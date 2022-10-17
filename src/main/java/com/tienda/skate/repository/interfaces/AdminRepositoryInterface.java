8b58e9e8491dad363604de4f1c96c2486c67e442
SofiGV <113567167+SofiGV@users.noreply.github.com>
2022-10-16 19:16:12 -0500

    Delete src directory

--- src/main/java/com/tienda/skate/repository/interfaces/AdminRepositoryInterface.java
+++
@@ -1,7 +0,0 @@
-package com.tienda.skate.repository.interfaces;
-
-import com.tienda.skate.model.Admin;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminRepositoryInterface extends CrudRepository<Admin, Integer> {
-}
--- src/main/java/com/tienda/skate/repository/interfaces/SkateRepositoryInterface.java
+++
@@ -1,7 +0,0 @@
-package com.tienda.skate.repository.interfaces;
-
-import com.tienda.skate.model.Skate;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SkateRepositoryInterface extends CrudRepository<Skate, Integer> {
-}
